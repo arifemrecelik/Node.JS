34bf88673dc139059aefd7a913e01ac1f7c2142b
arif.celik <ce.arifemre@gmail.com>
2018-08-06 10:48:36 +0300

    Initial commit

---
+++ notes-node/app.js
@@ -0,0 +1,12 @@
+console.log("Starting app.js");
+
+const fs = require('fs');
+const os = require('os');
+const notes = require('./notes.js');
+
+var user = os.userInfo();
+
+fs.appendFileSync('greetings.txt', 'Hello ' + user.username + '!');
+
+// Niye çalışmadı?
+// fs.appendFileSync('greetings.txt', 'Hello ${user.username}!');
