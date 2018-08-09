886c865d59f2e02cd3e9d73bfe98534a8e4c7602
arif.celik <ce.arifemre@gmail.com>
2018-08-09 17:41:23 +0300

    In progress

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -3,54 +3,71 @@ console.log('Starting notes.js');
 const fs = require('fs');
 
 var fetchNotes = () => {
-
+    try {
+        var noteString = fs.readFileSync('notes-data.json');
+        return JSON.parse(noteString);
+    }   catch (e) {
+        return []
+    }
 }
 
-var saveNotes = () => {
-    
+var saveNotes = (notes) => {
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
 }
 
 var addNote = (title, body) => {    
-    var notes = [];    
+    var notes = fetchNotes();
     var note = {
         title,
         body
-    }
-
-    try {
-        var noteString = fs.readFileSync('notes-data.json');
-        notes = JSON.parse(noteString);
-    }   catch (e) {
-
-    }
+    }   
 
     // var duplicateNotes = notes.filter((note) => {
     //     return note.title === title;
     // })
-
     var duplicateNotes = notes.filter((note) => note.title === title);
 
     if (duplicateNotes.length === 0) {
         notes.push(note);
-        fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+        saveNotes(notes);
+        return note;
     }
 }
 
-var getAll =  () => {
+var getAll = () => {
     console.log('Getting all notes');
 }
 
 var getNote = (title) => {
-    console.log('Getting note', title);
+    var notes = fetchNotes();
+    var filteredNotes = notes.filter((note) => note.title === title);
+    return filteredNotes[0];
 }
 
 var removeNote = (title) => {
-    console.log('Removing note', title);
+    var notes = fetchNotes();
+    var filteredNotes = notes.filter((note) => note.title !== title);
+    saveNotes(filteredNotes);
+
+    // Bu kısım neden videodaki gibi çalışmadı. (Removing a note - 19.ders / 7.dakika civarları)
+    return notes.length !== filteredNotes.length ? true : false;
+
+    // if (notes.length !== filteredNotes.length)
+    //     return true;
+    // else
+    //     return false;
+}
+
+var logNote = (note) => {
+    console.log('--');
+    console.log('Title:', note.title);
+    console.log('Body:', note.body);
 }
 
 module.exports = {
     addNote,
     getAll,
     getNote,
-    removeNote
+    removeNote,
+    logNote
 }
